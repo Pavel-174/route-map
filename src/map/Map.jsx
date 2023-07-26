f6131675e4e95c4ae4a7419bde4859e9d7842885
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-07-26 15:22:49 +0500

    leaflet plylines

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,22 +1,47 @@
 import React from 'react'
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
+import { MapContainer, TileLayer, ZoomControl, Polyline } from 'react-leaflet'
 
 
 const Map = () => {
 
+  const limeOptions = { color: 'lime' }
+  const blackOptions = { color: 'black' }
+  const redOptions = { color: 'red' }
+
+  const polyline1 = [
+    [59.84660399, 30.29496392],
+    [59.82934196, 30.42423701],
+    [59.83567701, 30.38064206],
+  ]
+
+  const polyline2 = [
+    [59.82934196, 30.42423701],
+    [59.82761295, 30.41705607],
+    [59.84660399, 30.29496392],
+  ]
+
+  const polyline3 = [
+    [59.83567701, 30.38064206],
+    [59.84660399, 30.29496392],
+    [59.82761295, 30.41705607],
+  ]
+
   return (
     <>
       <MapContainer 
-        center={[37.0902, -95.7129]} 
-        zoom={3} 
-        zoomControl={false} 
+        center={[59.82934196, 30.42423701]} 
+        zoom={10} 
+        zoomControl={true} 
         style={{ height: '100vh', width: '100%' }}
       >
         <TileLayer
           attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <ZoomControl position='topright'/>
+        {/* <ZoomControl position='topright'/> */}
+        <Polyline pathOptions={limeOptions} positions={polyline1} />
+        <Polyline pathOptions={blackOptions} positions={polyline2} />
+        <Polyline pathOptions={redOptions} positions={polyline3} />
       </MapContainer>
     </>
   )
