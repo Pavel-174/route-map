a46b4d0ed2f36da46f88effcee1c52b840f3f5b9
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-07-26 13:50:45 +0500

    leafleat.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import { Container } from "@material-ui/core";
 import Map from "./map/Map";
+import "leaflet/dist/leaflet.css";
 
 export default function App() {
   return (
