5b0d4b54f7da9e6735a17c94df1f24428d38dbfd
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-07-26 14:13:51 +0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.scss";
 import { Container } from "@material-ui/core";
 import Map from "./map/Map";
 import "leaflet/dist/leaflet.css";
 
 export default function App() {
+
+  
   return (
-    <Container disableGutters>
-      <Map />
-    </Container>
+    <>
+      <Container disableGutters maxWidth="sm">
+        <input type="radio" name="radAnswer"/> <span>Route №1</span>
+        <input type="radio" name="radAnswer"/> <span>Route №2</span>
+        <input type="radio" name="radAnswer"/> <span>Route №3</span>
+      </Container>
+      <Container disableGutters maxWidth="sm">
+       <Map />
+      </Container>
+    </>
   );
 }
